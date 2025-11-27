4e11d6f6ed65d26bdc515fab30da8aa45d097cb8
SangJinCode <youremail@example.com>
2025-11-27 12:12:17 -0800

    changed BASE_URL

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,9 +1,9 @@
 import axios from "axios"
 
-// const BASE_URL = import.meta.env.MODE === 'development'
-//     ? 'http://localhost:5001/api' : 'https://slack-clone-gamma-gules.vercel.app/api'
+const BASE_URL = import.meta.env.MODE === 'development'
+    ? 'http://localhost:5001/api' : 'https://slack-clone-gamma-gules.vercel.app/api'
 
-const BASE_URL = import .meta.env.VITE_API_BASE_URL
+// const BASE_URL = import .meta.env.VITE_API_BASE_URL
 
 // axios 인스턴스 생성
 export const axiosInstance = axios.create({
